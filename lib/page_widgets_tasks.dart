33b26747a3b4c7be3f0f91eca405a8da1eb4a4da
anmalk <anmalkov11@gmail.com>
2024-05-24 18:46:27 +0300

    fix bugs with new tasks type

--- lib/page_widgets_tasks.dart
+++ lib/page_widgets_tasks.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:EducationalApp/services/db.dart';
 import 'models/task_more_controller.dart';
 import 'widgets/helpiconbutton.dart';
-import 'widgets/textcategorystatefulwidget.dart';
-import 'widgets/categoryiconbutton.dart';
 import 'widgets/functions.dart';
 import 'models/object_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -19,151 +17,105 @@ Widget buildTasksPage(BuildContext context, Map<String, dynamic>? pageData) {
       child: Text('Page data is empty.'),
     );
   }
-  return SingleChildScrollView(
-    padding: EdgeInsets.all(20.0),
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        SizedBox(height: 5),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            HelpIconButton(onPressed:() {
 
-            },
-              helpImageUrlData: pageData['Task page']['help_image'],), // Ваша кнопка HelpButton
-          ],
-        ),
-        SizedBox(height: 0),
-        Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10), // Задайте радиус для создания закругленных углов
-            border: Border.all(
-              color: Colors.black, // Задайте цвет рамки
-              width: 2, // Задайте толщину рамки
-            ),
+  return Scaffold(
+    appBar: AppBar(
+      title: Text('Game'),
+    ),
+    body: SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(height: 20),
+          Text(
+            'Find the matching image',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
           ),
-          child: Container(
-
-            padding: EdgeInsets.all(10), // Отступы для контейнера с изображениями
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Image.network(
-                      TaskController_more.tasks[0].objectsDataDiffCategories[0].values.elementAt(0)['url'],
-                      width: 190,
-                      height: 190,
-                    ),
-                    Image.network(
-                      TaskController_more.tasks[0].objectsDataDiffCategories[0].values.elementAt(1)['url'],
-                      width: 190,
-                      height: 190,
-                    ),
-                  ],
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              for (int i = 0; i < 3; i++)
+                GestureDetector(
+                  onTap: () {
+                  },
+                  child: Image.network(
+                    TaskController_more.tasks[TaskController_more.currentTaskIndex].images[i].url,
+                    width: 100,
+                    height: 100,
+                  ),
                 ),
-                SizedBox(height: 5),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Image.network(
-                      TaskController_more.tasks[0].objectsDataDiffCategories[0].values.elementAt(2)['url'],
-                      width: 190,
-                      height: 190,
-                    ),
-                    Image.network(
-                      'https://img.lovepik.com/element/40117/2354.png_860.png',
-                      width: 150,
-                      height: 150,
-                    ),
-                  ],
-                ),
-              ],
-            ),
+            ],
           ),
-        ),
-        SizedBox(height: 10),
-        Wrap(
-          alignment: WrapAlignment.center,
-          spacing: 8.0,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                //print(TaskController_more.tasks[0].objectsDataDiffCategories[0].values.elementAt(2)['name_category']);
-                //print(TaskController_more.tasks[0].objectsDataOneCategory[0].values.elementAt(1)['name_category']);
-
-                if(TaskController_more.tasks[0].objectsDataDiffCategories[0].values.elementAt(0)['name_category'] != TaskController_more.tasks[0].objectsDataOneCategory[0].values.elementAt(2)['name_category'])
-                {
-
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => ResultPage()),
-                  );
-                }
-              },
-              child: Image.network(
-                TaskController_more.tasks[0].objectsDataOneCategory[0].values.elementAt(0)['url'],
-                width: 150,
-                height: 150,
-              ),
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10), // Задайте радиус для создания закругленных углов кнопки
+          SizedBox(height: 20),
+          Text(
+            'Select one of these:',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              for (int i = 3; i < 6; i++)
+                GestureDetector(
+                  onTap: () {
+                    if (TaskController_more.tasks[TaskController_more.currentTaskIndex].category ==
+                        TaskController_more.tasks[TaskController_more.currentTaskIndex].images[i].category) {
+                      _showCorrectAnimation(context);
+                    } else {
+                      _showIncorrectAnimation(context);
+                    }
+                  },
+                  child: Image.network(
+                    TaskController_more.tasks[TaskController_more.currentTaskIndex].images[i].url,
+                    width: 100,
+                    height: 100,
+                  ),
                 ),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                if(TaskController_more.tasks[0].objectsDataDiffCategories[0].values.elementAt(1)['name_category'] == TaskController_more.tasks[0].objectsDataOneCategory[0].values.elementAt(2)['name_category'])
-                {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => ResultPage()),
-                  );
-                }
-              },
-              child: Image.network(
-                TaskController_more.tasks[0].objectsDataOneCategory[0].values.elementAt(1)['url'],
-                width: 150,
-                height: 150,
-              ),
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10), // Задайте радиус для создания закругленных углов кнопки
-                ),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                if(TaskController_more.tasks[0].objectsDataDiffCategories[0].values.elementAt(2)['name_category'] != TaskController_more.tasks[0].objectsDataOneCategory[0].values.elementAt(2)['name_category'])
-                  {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => ResultPage()),
-                    );
-                  }
-              },
-              child: Image.network(
-                TaskController_more.tasks[0].objectsDataOneCategory[0].values.elementAt(2)['url'],
-                width: 150,
-                height: 150,
-              ),
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10), // Задайте радиус для создания закругленных углов кнопки
-                ),
-              ),
-            ),
-            // Добавьте столько изображений, сколько вам нужно
-          ],
-        ),
-        SizedBox(height: 20),
-        ProgressBarWidget(progressBarData: pageData['Task page']['Progress Bar'], currentTaskIndex: TaskController_more.currentTaskIndex, totalImages: TaskController_more.length),
-      ],
+            ],
+          ),
+        ],
+      ),
     ),
   );
 }
 
+void _showCorrectAnimation(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('Correct!'),
+        content: Text('You found the matching image.'),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('OK'),
+          ),
+        ],
+      );
+    },
+  );
+}
 
+void _showIncorrectAnimation(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('Incorrect!'),
+        content: Text('Sorry, try again.'),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('OK'),
+          ),
+        ],
+      );
+    },
+  );
+}
--- lib/tasks_page.dart
+++ lib/tasks_page.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'services/storage_service.dart';  // Импортируйте ваш StorageService
@@ -33,73 +34,24 @@ class _TasksPageState extends State<TasksPage> {
     TaskController_more.currentValue = 0;
 
     super.initState();
-    print('Выбранная категория: ${widget.name}');
+    //print('Выбранная категория: ${widget.name}');
     DatabaseService.initFirebase();
     jsonData = {};
     fetchData();
 
+    TaskController_more.fetchTasks().then((tasks) {
+      TaskController_more.tasks = tasks;
+    });
 
-    TaskController_more.tasks = [
-      Task(
-        id: '1',
-        idCategory: 'category1',
-        objectsDataDiffCategories: [
-          {
-            Object(id_objects: '1', name: 'Object 1', name_categories: 'category1', url: 'https://example.com/image1.jpg'): {
-              'id_objects': '1',
-              'name': 'Object 1',
-              'name_categories': 'category1',
-              'url': 'https://img.povar.ru/mobile/8b/4e/89/f5/uzbekskii_plov-4860.jpg'
-            },
-            Object(id_objects: '2', name: 'Object 2', name_categories: 'category1', url: 'https://example.com/image2.jpg'): {
-              'id_objects': '2',
-              'name': 'Object 2',
-              'name_categories': 'category1',
-              'url': 'https://static.insales-cdn.com/images/products/1/2038/401188854/kvarcevii_pesok.jpg'
-            },
-            Object(id_objects: '3', name: 'Object 3', name_categories: 'category1', url: 'https://example.com/image2.jpg'): {
-              'id_objects': '3',
-              'name': 'Object 3',
-              'name_categories': 'category1',
-              'url': 'https://www.tg-stroy.ru/wp-content/uploads/2021/08/Rectangle-25-2-570x360.jpg'
-            },
-          },
-        ],
-        objectsDataOneCategory: [
-          {
-            Object(id_objects: '4', name: 'Object 4', name_categories: 'category2', url: 'https://upload.wikimedia.org/wikipedia/commons/0/0c/Two-parts_stone_nikogda_takih_ne_videl_vot.JPG'): {
-              'id_objects': '4',
-              'name': 'Object 4',
-              'name_categories': 'category2',
-              'url': 'https://upload.wikimedia.org/wikipedia/commons/0/0c/Two-parts_stone_nikogda_takih_ne_videl_vot.JPG'
-            },
-            Object(id_objects: '5', name: 'Object 5', name_categories: 'category1', url: 'https://aif-s3.aif.ru/images/019/734/a3e931c399f2ddb1e74ccdb2bda0b2e7.jpg'): {
-              'id_objects': '5',
-              'name': 'Object 5',
-              'name_categories': 'category1',
-              'url': 'https://aif-s3.aif.ru/images/019/734/a3e931c399f2ddb1e74ccdb2bda0b2e7.jpg'
-            },
-            Object(id_objects: '6', name: 'Object 6', name_categories: 'category2', url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYotOoL8JNtaChgVqbQDumZEuf1lhFO5Qk4UxHoFFRAw&s'): {
-              'id_objects': '6',
-              'name': 'Object 6',
-              'name_categories': 'category2',
-              'url': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYotOoL8JNtaChgVqbQDumZEuf1lhFO5Qk4UxHoFFRAw&s'
-            },
-          },
-        ],
-      ),
-      // Другие задачи здесь...
-    ];
 
-    // Вызываем метод загрузки задач из Firestore
-    TaskController_more.fetchTasksFromFirestore().then((_) {
-      // Делаем что-то после загрузки задач
-      TaskController_more.length = TaskController_more.tasks.length;
       print('Длина:');
       print(TaskController_more.length);
       print(TaskController_more.tasks);
       print('Печать');
-    });
+      for (Task task in TaskController_more.tasks) {
+        print('Task ID: ${task.images[0].url}');
+      }
+
 
     print(TaskController_more.currentTaskIndex);
 
